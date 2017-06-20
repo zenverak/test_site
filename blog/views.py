1fae50a92a70689b8c05b8ff2a10947dcbef0273
Chris Jones <zenverak@gmail.com>
2017-06-20 13:40:26 +0000

    added html stuff

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
 # Create your views here.
